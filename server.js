0a1d5a491e82709d0cb962d915cb22785e365da9
Siva Seshu <48062329+rsivaseshu@users.noreply.github.com>
2023-01-06 20:09:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-'use strict';
+s'use strict';
 
 const express = require('express');
 
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('DevOps world');
 });
 
 app.listen(PORT, HOST);
